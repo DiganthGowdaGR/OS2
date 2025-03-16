08ca854e7dd7e1c76d706655b15d107ba0b40a76
Sharath GR <160706150+DiganthGowdaGR@users.noreply.github.com>
2025-03-16 17:21:34 +0530

    Create step2_memory.c

---
+++ step2_memory.c
@@ -0,0 +1,21 @@
+•	#include <stdio.h>
+•	#include <stdlib.h>
+•	#include <unistd.h>
+•	
+•	void print_memory_map(pid_t pid) {
+•	    char cmd[50];
+•	    sprintf(cmd, "cat /proc/%d/maps", pid);  // Prepare the command
+•	    printf("\nMemory map of PID %d:\n", pid);
+•	    system(cmd);  // Execute the command
+•	}
+•	
+•	int main() {
+•	    pid_t pid = getpid();
+•	    printf("Process ID: %d\n", pid);
+•	    
+•	    // Print memory segments of the current process
+•	    print_memory_map(pid);
+•	
+•	    return 0;
+•	}
+
